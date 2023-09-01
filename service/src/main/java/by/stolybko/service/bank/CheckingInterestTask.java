908c70711df4dddfe5e42ace2699c9cc5d6660ce
Andrey <thekanas@yandex.ru>
2023-09-02 00:16:32 +0300

    the verification of interest accrual has been implemented

---
+++ service/src/main/java/by/stolybko/service/bank/CheckingInterestTask.java
@@ -0,0 +1,35 @@
+package by.stolybko.service.bank;
+
+import by.stolybko.service.service.AccountService;
+import by.stolybko.service.util.PropertiesManager;
+
+import java.time.LocalDate;
+
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+
+public class CheckingInterestTask implements Runnable {
+    private final AccountService accountService = AccountService.getInstance();
+
+    private boolean interestAccrued = false;
+    @Override
+    public void run() {
+        LocalDate currentDay = LocalDate.now();
+        LocalDate lastDay = currentDay.with(lastDayOfMonth());
+        System.out.println("\n============service message============");
+        System.out.println("checking interest accrual");
+        if (currentDay == lastDay) {
+            if (!interestAccrued) {
+                accountService.accrueInterest(Double.valueOf(PropertiesManager.get("percent")));
+                System.out.println("interest accrued");
+                interestAccrued = true;
+            } else {
+                System.out.println("interest has already been accrued today");
+            }
+
+        } else {
+            System.out.println("interest accrual is not required");
+            interestAccrued = false;
+        }
+        System.out.println("============service message============\n");
+    }
+}
--- service/src/main/java/by/stolybko/service/bank/ClientBank.java
+++ service/src/main/java/by/stolybko/service/bank/ClientBank.java
@@ -2,11 +2,20 @@ package by.stolybko.service.bank;
 
 import by.stolybko.database.entity.Bank;
 
-public class ClientBank {
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class ClientBank {
+    private static final long DELAY = 30;
     public static void start() {
         Bank bank = BankFactory.getBank("CleverBank");
 
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new CheckingInterestTask(), DELAY, DELAY, TimeUnit.SECONDS);
+
         new Menu(new BankManagement(bank)).start();
+
+
     }
 }
